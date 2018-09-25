2ba528429a9aacdd77e71800ac396656dc20fc61
sugarshin <shinsugar@gmail.com>
2018-09-25 13:29:04 +0900

    Add Authentication

--- src/origins.ts
+++ src/origins.ts
@@ -1,2 +1,3 @@
 export const RESTAURANTS_SERVICE_ORIGIN: string = 'https://remap-restaurants-production.mybluemix.net'
 export const STOCKS_SERVICE_ORIGIN: string = 'https://remap-stocks-production.mybluemix.net'
+export const AUTHENTICATION_SERVICE_ORIGIN: string = 'https://remap-authentication-production.mybluemix.net'
--- test.ts
+++ test.ts
@@ -1,16 +1,20 @@
 import test from 'ava'
 import nock from 'nock'
-import { Restaurants, Stocks } from './src'
-import { RESTAURANTS_SERVICE_ORIGIN, STOCKS_SERVICE_ORIGIN } from './src/origins'
+import { Restaurants, Stocks, Authentication } from './src'
+import { RESTAURANTS_SERVICE_ORIGIN, STOCKS_SERVICE_ORIGIN, AUTHENTICATION_SERVICE_ORIGIN } from './src/origins'
 
 test.before(() => {
   nock(RESTAURANTS_SERVICE_ORIGIN)
     .get('/?latitude=90294.3&longitude=243.23626')
-    .reply(200, []);
+    .reply(200, [])
 
   nock(STOCKS_SERVICE_ORIGIN)
     .get('/')
-    .reply(200, []);
+    .reply(200, [])
+
+  nock(AUTHENTICATION_SERVICE_ORIGIN)
+    .post('/', { id_token: 'aa0s9d80f98ds' })
+    .reply(200, {})
 })
 
 test('Restaurants - getList', async t => {
@@ -21,4 +25,9 @@ test('Restaurants - getList', async t => {
 test('Stocks - getList', async t => {
   const res = await Stocks.getList({}, 'sadf9s7a9870s9d80fds8f09d')
   t.deepEqual(res, [])
+})
+
+test('Authentication - authenticate', async t => {
+  const res = await Authentication.authenticate('aa0s9d80f98ds')
+  t.deepEqual(res, {})
 })
